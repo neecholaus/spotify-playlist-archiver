44344b8afb046be8f17b5eaefd352db6c9c6f532
neecholaus <thisisnickadam@gmail.com>
2020-12-22 15:26:08 -0800

    script now parses auth fragment from persistent file

---
+++ archive-playlists.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { exit } = require('process');
+
+const authorizationFilePath = './account_access.json';
+
+// ensure auth file exists
+if (!fs.existsSync(authorizationFilePath)) {
+    console.log('account authorization not found, please run through account authorization');
+    exit;
+}
+
+// parse auth file
+let authorizationFragment;
+try {
+    authorizationFragment = JSON.parse(fs.readFileSync(authorizationFilePath, 'utf-8'));
+} catch (e) {
+    console.log('authorization could not be parsed, please run through account authorization again');
+    return;
+}
+
+console.log(authorizationFragment);
