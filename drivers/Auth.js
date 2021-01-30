2b90cf8da86e889258fc380bf0c63e08a210684d
neecholaus <thisisnickadam@gmail.com>
2021-01-29 23:00:20 -0800

    added loop so you can go through the auth process a few different ways without being kicked out at every little error

--- drivers/Auth.js
+++ drivers/Auth.js
@@ -1,6 +1,70 @@
+const child = require('child_process');
 const fs = require('fs');
+const path = require('path');
+
+const {log} = require('./Log');
+const baseSpotify = require('./Spotify');
 
 class Auth {
+    static async boot(dieOnFail=false) {
+        /** @var object */
+        let accountAccess;
+
+        /** @var int */
+        let authAttemps = 0;
+
+        while (!accountAccess && authAttemps <= 2) {
+            /** @var bool */
+            let mustRunThroughAuth = false;
+
+            try {
+                accountAccess = this.getAccountAccess();
+                log('token was found', 'auth');
+            } catch (e) {
+                log('token could not be accessed', 'auth');
+                mustRunThroughAuth = true;
+            }
+
+            if (mustRunThroughAuth) {
+                // allow 2 attempts, on third show error and exit
+                if (authAttemps == 2) {
+                    log('failed to authenticate twice, shutting down', 'auth');
+                    process.exit();
+                }
+
+                log('starting auth server', 'auth');
+
+                // start server
+                child.execFileSync(path.resolve(__dirname, '../scripts/start-server.sh'));
+            }
+
+            authAttemps++;
+        }
+
+        // hit spotify and check validity of token
+        log('checking token with spotify', 'auth');
+
+        let spotify = new baseSpotify(accountAccess);
+
+        let userData = await spotify.getUser();
+
+        if (! userData) {
+            log('spotify rejected the token', 'auth');
+            log('deleting bad token and trying again', 'auth');
+
+            if (!dieOnFail) {
+                // token was bad for some reason,
+                // remove existing access json so we run through auth server again
+                fs.unlinkSync(path.resolve(__dirname, '../account_access.json'));
+                this.boot(true);
+            }
+        } else {
+            log(`you are authenticated as "${userData.display_name}"`, 'auth');
+
+            // todo - call next step in app?
+        }
+    }
+
     /** 
      * @throws Error
      * @returns object|void */
@@ -13,7 +77,7 @@ class Auth {
      * @returns object|void
      */
     static _readAndParseAccountAccess() {
-        if (! fs.existsSync('account_access.json')) {
+        if (! fs.existsSync(path.resolve(__dirname, '../account_access.json'))) {
             throw Error('account access file does not exist');
         }
 
--- kernel.js
+++ kernel.js
@@ -1,62 +1,10 @@
-console.log('== Spotify Playlist Archiver ==');
-
-const child = require('child_process');
-const fs = require('fs');
-const path = require('path');
-
 const {log} = require('./drivers/log');
 const auth = require('./drivers/Auth');
-const baseSpotify = require('./drivers/Spotify');
+
+log('== Spotify Playlist Archiver ==');
 
 log('attempting to pull existing access token', 'auth');
 
 (async function () {
-
-    /** @var object */
-    let accountAccess;
-
-    /** @var int */
-    let authAttemps = 0;
-
-    /** @var bool */
-    let mustRunThroughAuth = false;
-
-    while (!accountAccess && authAttemps <= 2) {
-        try {
-            accountAccess = auth.getAccountAccess();
-            log('token was found', 'auth');
-        } catch (e) {
-            log('token could not be accessed', 'auth');
-            mustRunThroughAuth = true;
-        }
-
-        if (mustRunThroughAuth) {
-            // allow 2 attempts, on third show error and exit
-            if (authAttemps == 2) {
-                log('failed to authenticate twice, shutting down', 'auth');
-                process.exit();
-            }
-
-            log('starting auth server', 'auth');
-
-            // start server
-            child.execFileSync(path.resolve(__dirname, './scripts/start-server.sh'));
-        }
-
-        authAttemps++;
-    }
-
-    // hit spotify and check validity of token
-    log('checking token with spotify', 'auth');
-
-    let spotify = new baseSpotify(accountAccess);
-
-    let userData = await spotify.getUser();
-
-    if (! userData) {
-        log('spotify rejected the token', 'auth');
-    } else {
-        log(`you are authenticated as "${userData.display_name}"`, 'auth');
-    }
-
+    auth.boot()
 })();
