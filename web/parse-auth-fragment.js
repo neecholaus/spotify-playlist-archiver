730c299b90703b920cdcc55242075036a0dc2a6e
neecholaus <thisisnickadam@gmail.com>
2021-01-31 13:32:26 -0800

    failed authentication handled, better display of message on redirect

--- web/parse-auth-fragment.js
+++ web/parse-auth-fragment.js
@@ -1,9 +1,20 @@
-fetch("/store-user-token", {
-    headers: {
-        "Content-Type": "application/json"
-    },
-    method: "POST",
-    body:JSON.stringify({
-        token:window.location.hash
-    })
-});
+let redirectParams = new URLSearchParams(window.location.search);
+
+if (redirectParams.has('error')) {
+    document.write(buildHeader(redirectParams.get('error')));
+} else {
+    document.write(buildHeader('authenticated'));
+    fetch("/store-user-token", {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body:JSON.stringify({
+            token:window.location.hash
+        })
+    });
+}
+
+function buildHeader(text) {
+    return `<h1 style="text-align:center;font-weight:bolder">${text}</h1>`;
+};
