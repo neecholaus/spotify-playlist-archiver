3592249ea7c4980cb7ecd5ae7a23bdcac74fc4e6
neecholaus <thisisnickadam@gmail.com>
2021-03-28 13:22:16 -0700

    archiver now emits liked songs and playlist tracks to their own file in ./archives

--- drivers/Archiver.js
+++ drivers/Archiver.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const {log} = require('./Log');
 const baseSpotify = require('./Spotify');
 
@@ -16,15 +17,34 @@ class Archiver {
             likedSongs = await this.spotify.fetchAllLikedSongs(),
             playlists = await this.spotify.fetchAllPlaylists();
 
+        const outputPath = './archives';
+
+        // clear all output and recreate dir
+        if (fs.existsSync(outputPath)) {
+            fs.rmdirSync(outputPath, {
+                recursive: true
+            });
+        }
+        fs.mkdirSync(outputPath);
+
+        // archive all liked songs
         likedSongs.map(track => {
-            // echo into file
+            track = track.track;
+
+            const likedSongFilepath = outputPath + '/Liked\ Songs';
+
+            fs.appendFileSync(likedSongFilepath, "\n" + track.artists[0].name + ' - ' + track.name);
         });
 
         playlists.map(async playlist => {
+            const playlistFilePath = outputPath + '/' + playlist.name;
+
             const tracks = await this.spotify.fetchAllTracksInPlaylist(playlist.id);
 
-            tracks.forEach(x => {
-                // echo into file
+            tracks.forEach(track => {
+                track = track.track;
+
+                fs.appendFileSync(playlistFilePath, "\n" + track.artists[0].name + ' - ' + track.name)
             });
         });
     }
